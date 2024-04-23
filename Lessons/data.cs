d1506b3bf97d02d16c74a5d1053f9cef1b9ca9ad
ACE dayoooooooo <88769732+Jayodom-Piamthai@users.noreply.github.com>
2024-04-23 20:47:19 +0700

    what

---
+++ Lessons/data.cs
@@ -0,0 +1,19 @@
+namespace Lesson2
+{
+    class program
+    {
+        static void Main(strings[] args)
+        { //main code activation
+            string name = 'ace';
+            float doubling  = 0.0f;
+            int cycle = 23;
+            int assignable;
+            assignable = 0;
+
+
+
+            Console.Writeline(assignable);
+            Console.ReadLine();
+        }
+    }
+}
