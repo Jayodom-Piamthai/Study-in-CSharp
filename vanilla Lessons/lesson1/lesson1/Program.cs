f2daa5b602182e6b1b1d5d722c350366d858db17
ACE! <88769732+Jayodom-Piamthai@users.noreply.github.com>
2025-04-22 17:19:12 +0700

    vanilla 1

---
+++ vanilla Lessons/lesson1/lesson1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "sup world";
+            Console.WriteLine(message);
+        }
+    }
+}
